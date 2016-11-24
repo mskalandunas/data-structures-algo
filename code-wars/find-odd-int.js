0ab8476b27d40211e12487c8c5815eb030182f9b
Mike Skalandunas <mskalandunas@gmail.com>
2016-11-23 22:39:43 -0800

    add find odd

---
+++ code-wars/find-odd-int.js
@@ -0,0 +1,22 @@
+/*
+
+Given an array, find the int that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+*/
+
+function findOdd(arr) {
+  arr.sort();
+
+  for (var i = 0; i < arr.length;) {
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i, 2);
+    } else {
+      return arr[0];
+    };
+  };
+};
+
+input = [ 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 ]
+findOdd(input);
